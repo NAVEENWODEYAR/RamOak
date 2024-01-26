a3de69056c7f4f03c9b7b0b391a9bf689b24b761
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-26 16:59:15 +0530

    75 Republic day

--- src/main/java/com/ram/controller/MyController.java
+++ src/main/java/com/ram/controller/MyController.java
@@ -2,10 +2,12 @@ package com.ram.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/ram")
 public class MyController {
 	
 	@GetMapping
