b40a561b541d4080cecf54eb1065613e6075b6c4
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-27 14:19:15 +0530

    entity defined

---
+++ src/main/java/com/ram/entity/UserInfo.java
@@ -0,0 +1,25 @@
+package com.ram.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "User_Info")
+public class UserInfo {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Integer userId;
+    private String userName;
+    private String userMail;
+    private String userRoles;
+}
