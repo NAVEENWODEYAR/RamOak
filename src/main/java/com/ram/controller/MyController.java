46cd63321879be3488b089f45222c3184c963edf
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-27 12:57:38 +0530

    sl4j error still exists

--- src/main/java/com/ram/controller/MyController.java
+++ src/main/java/com/ram/controller/MyController.java
@@ -6,17 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/ram")
-@Slf4j
 public class MyController {
 	
 	@GetMapping
 	@ResponseStatus(code = HttpStatus.FOUND)
 	public String greet() {
-		log.info("Jai Sri Ram");
 		return "Jai Sri Ram";
 	}
 }
