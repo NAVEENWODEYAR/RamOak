d144348d5f888110368fc2234c91157b6a5ee874
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-27 14:53:14 +0530

    basic classes creation completed

--- src/main/java/com/ram/service/UserInfoDetails.java
+++ src/main/java/com/ram/service/UserInfoDetails.java
@@ -35,44 +35,37 @@ public class UserInfoDetails implements UserDetails {
     
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-	// TODO Auto-generated method stub
-	return null;
+	return authorities;
     }
 
     @Override
     public String getPassword() {
-	// TODO Auto-generated method stub
-	return null;
+	return userPassword;
     }
 
     @Override
     public String getUsername() {
-	// TODO Auto-generated method stub
-	return null;
+	return userName;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-	// TODO Auto-generated method stub
-	return false;
+	return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-	// TODO Auto-generated method stub
-	return false;
+	return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-	// TODO Auto-generated method stub
-	return false;
+	return true;
     }
 
     @Override
     public boolean isEnabled() {
-	// TODO Auto-generated method stub
-	return false;
+	return true;
     }
 
 }
--- src/main/java/com/ram/service/UserService.java
+++ src/main/java/com/ram/service/UserService.java
@@ -1,5 +1,6 @@
 package com.ram.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +24,22 @@ public class UserService implements UserDetailsService{
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 	Optional<UserInfo> user = userRepo.findByUserName(username);
-	return user.map(UserInfoDetails::new);
+	return user.map(UserInfoDetails::new)
+		.orElseThrow(()->new UsernameNotFoundException("User not found,"+username));
+    }
+    
+    public String addUser(UserInfo userInfo) {
+	userInfo.setUserPassword(passwordEncoder.encode(userInfo.getUserPassword()));
+	userRepo.save(userInfo);
+	return "User save successfully";
+    }
+    
+    public List<UserInfo> getUsers(){
+	return userRepo.findAll();
+    }
+    
+    public UserInfo getUser(Integer userId) {
+	return userRepo.findById(userId).get();
     }
 
    
