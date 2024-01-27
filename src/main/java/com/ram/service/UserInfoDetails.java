7b32ca4336a79b7ab2129e8ec5eefa0f9fbe5c52
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-27 19:59:32 +0530

    code copied from the remote repo

--- src/main/java/com/ram/service/UserInfoDetails.java
+++ src/main/java/com/ram/service/UserInfoDetails.java
@@ -1,71 +1,60 @@
 package com.ram.service;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.ram.entity.UserInfo;
+
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
-
-import com.ram.entity.UserInfo;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserInfoDetails implements UserDetails {
-
-    private String userName=null;
-    private String userPassword=null;
+    String userName=null;
+    String password = null;
     List<GrantedAuthority> authorities;
-    
-    public UserInfoDetails(UserInfo userInfo) {
-	this.userName = userInfo.getUserName();
-	this.userPassword = userInfo.getUserPassword();
-	authorities = Arrays.stream(userInfo.getUserRoles().split(" "))
-				.map(SimpleGrantedAuthority::new)
-				.collect(Collectors.toList());
+
+    public UserInfoDetails(UserInfo userInfo){
+       userName= userInfo.getUserName();
+       password= userInfo.getUserPassword();
+       authorities= Arrays.stream(userInfo.getUserRoles().split(","))
+               .map(SimpleGrantedAuthority::new)
+               .collect(Collectors.toList());
     }
-    
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-	return authorities;
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-	return userPassword;
+        return password;
     }
 
     @Override
     public String getUsername() {
-	return userName;
+        return userName;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-	return true;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-	return true;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-	return true;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-	return true;
+        return true;
     }
-
 }
--- src/main/java/com/ram/service/UserService.java
+++ src/main/java/com/ram/service/UserService.java
@@ -1,46 +1,41 @@
 package com.ram.service;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.ram.entity.UserInfo;
 import com.ram.repo.UserRepo;
 
-@Service
-public class UserService implements UserDetailsService{
+import java.util.List;
+import java.util.Optional;
 
+@Service
+public class UserService implements UserDetailsService {
     @Autowired
-    private UserRepo userRepo;
+    private UserRepo userInfoRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
-    
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-	Optional<UserInfo> user = userRepo.findByUserName(username);
-	return user.map(UserInfoDetails::new)
-		.orElseThrow(()->new UsernameNotFoundException("User not found,"+username));
+        Optional<UserInfo> userInfo = userInfoRepository.findByUserName(username);
+        return userInfo.map(UserInfoDetails::new)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found"+username));
     }
-    
-    public String addUser(UserInfo userInfo) {
-	userInfo.setUserPassword(passwordEncoder.encode(userInfo.getUserPassword()));
-	userRepo.save(userInfo);
-	return "User save successfully";
+    public String addUser(UserInfo userInfo){
+        userInfo.setUserPassword(passwordEncoder.encode(userInfo.getUserPassword()));
+        userInfoRepository.save(userInfo);
+        return "User added successfully";
     }
-    
-    public List<UserInfo> getUsers(){
-	return userRepo.findAll();
+    public List<UserInfo> getAllUser(){
+         return userInfoRepository.findAll();
     }
-    
-    public UserInfo getUser(Integer userId) {
-	return userRepo.findById(userId).get();
+    public UserInfo getUser(Integer id){
+        return userInfoRepository.findById(id).get();
     }
-
-   
 }
