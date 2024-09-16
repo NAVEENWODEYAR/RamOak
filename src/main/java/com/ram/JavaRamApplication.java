fb2b56e057239d27f16b65a6013d29957a3b41e7
Naveen <naveenkrishnawodeyar@gmail.com>
2024-09-16 22:51:16 +0530

    logging added

--- src/main/java/com/ram/JavaRamApplication.java
+++ src/main/java/com/ram/JavaRamApplication.java
@@ -1,7 +1,9 @@
 package com.ram;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -12,5 +14,16 @@ public class JavaRamApplication {
 		SpringApplication.run(JavaRamApplication.class, args);
 		System.out.println("Jai Sri Ram.,");
 	}
+	
+	@Bean
+	public CommandLineRunner printJavaVersion() {
+		return args -> {
+			String javaVersion = System.getProperty("java.version");
+			System.out.println("\n#############################\n");
+			System.out.println("JAVA VERSION: " + javaVersion);
+			System.out.println("\n#############################\n");
+		};
+	}
+
 
 }
